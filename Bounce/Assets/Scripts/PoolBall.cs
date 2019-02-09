b544acdbf7578720d2ad3ad7bc018bd0e2974c23
gareau <thilo@asablackbox.com>
2019-02-09 01:24:55 -0800

    Billiard 2nd Update

--- Bounce/Assets/Scripts/PoolBall.cs
+++ Bounce/Assets/Scripts/PoolBall.cs
@@ -14,18 +14,34 @@ public class PoolBall : MonoBehaviour
         {
             for(int j = 0; j< 4 - i; j ++)
             {
-                Instantiate<GameObject>(Ball).transform.position = pos.position + new Vector3(-0.1f,0.0f, 0.75f) + new Vector3((float)((j +0.5*i)*0.015), 0.016f, (float)(-i*0.5*0.03)*(float)Math.Sqrt(3));
+                Instantiate<GameObject>(Ball).transform.position = pos.position + new Vector3((float)(-1.5*0.06),0.0f, 0.7f) + new Vector3((float)((j +0.5*i)*0.06), (float)(0.5*0.06), (float)(-i*0.5*0.06)*(float)Math.Sqrt(3));
             }
         }
     }
+
+    private int k = 0;
     /// <summary>
     /// Use this method to dispense the item.
     /// </summary>
     /// <param name="itemNumber">ID of the item in the </param>
     public void Dispense(int itemNumber)
     {
-        // Dispense an object here using the Instantiate<> function
-        Ball(inventoryItems[itemNumber],dispensePosition);
+        if(itemNumber == 2)
+        {
+            k = 1;
+        }
+        if (itemNumber == 0 && k == 1)
+        {
+            // Dispense an object here using the Instantiate<> function
+            Ball(inventoryItems[itemNumber], dispensePosition);
+            k = 0;
+        }
+        if(itemNumber == 1 && k == 1)
+        {
+            Instantiate<GameObject>(inventoryItems[itemNumber]).transform.position = dispensePosition.position + new Vector3(0.0f, (float)(0.5 * 0.06), -0.7f);
+            k = 0;
+        }
+       
         Debug.Log(itemNumber);
     }
 }
