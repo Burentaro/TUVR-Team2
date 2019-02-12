b53a0f2017725d9dfc099858a0eff6c5d2e0bd7d
gareau <thilo@asablackbox.com>
2019-02-12 01:04:27 -0800

    Presentation day update

---
+++ Bowling and Billiard/Assets/Scripts/BowlingBall.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+//            ballPool[m] = Instantiate<GameObject>(Ball, pos.position + new Vector3((float)(-1.5 * 0.06), 0.0f, 0.7f) + new Vector3((float)((j + 0.5 * i) * 0.06), (float) (0.5 * 0.06), (float) (-i* 0.5 * 0.06) * (float) Math.Sqrt(3)), pos.rotation);
+
+public class BowlingBall : MonoBehaviour
+{
+    public GameObject[] inventoryItems;      // List of items that are in the dispenser
+    public Transform dispensePosition;  // Position where the items were come out
+    public GameObject pinPrefab;
+    public GameObject bballPrefab;
+
+    private void BBall(GameObject BBall, Transform pos)
+    {
+
+        
+    }
+
+    private int k = 0;
+    /// <summary>
+    /// Use this method to dispense the item.
+    /// </summary>
+    /// <param name="itemNumber">ID of the item in the </param>
+    public void Dispense(int itemNumber)
+    {
+        GameObject[] Bowling = inventoryItems;
+
+        if (itemNumber == 2)
+        {
+            k = 1;
+        }
+        if (itemNumber == 0 && k == 1)
+        {
+            int m = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4 - i; j++)
+                {
+                    Bowling[m] = Instantiate<GameObject>(pinPrefab, dispensePosition.position + new Vector3((float)(-1.5 * 0.25), 0.0f, 8.7f) + new Vector3((float)((j + 0.5 * i) * 0.25), (float)(0.5 * 0.25), (float)(-i * 0.5 * 0.25) * (float)Math.Sqrt(3)), dispensePosition.rotation);
+                    m++;
+                }
+            }
+            k = 0;
+        }
+        if (itemNumber == 1 && k == 1)
+        {
+            Bowling[0] = Instantiate<GameObject>(bballPrefab, dispensePosition.position + new Vector3(-1.0f, (float)(0.5 * 0.5), -11.5f),dispensePosition.rotation);
+            k = 0;
+        }
+
+        if (itemNumber == 3 && k == 1)
+        {
+            for (int n = 0; n <= 11; n++)
+            {
+                Destroy(Bowling[n], 0.0f);
+            }
+            k = 0;
+        }
+
+        Debug.Log(itemNumber);
+    }
+}
---
+++ Bowling and Billiard/Assets/Scripts/Remote.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Remote : MonoBehaviour
+{
+    public Cannon cannon;           // Reference to the Cannon
+    private bool isHeld = false;    // A flag that we user to know if the remote is being held by the player
+    
+    /// <summary>
+    /// A simple toggle method to set whether the remote is being held or not. 
+    /// Attach this to the SteamVR Throwable component.
+    /// </summary>
+    public void HoldRemote()
+    {
+        bool oldHoldValue = isHeld;
+        if (oldHoldValue)
+        {
+            isHeld = false;
+        }
+        else
+        {
+            isHeld = true;
+        }
+    }
+
+    public void Fire()
+    {
+        // TO-DO!!
+        // Create script to only call the Shoot method on the cannon if remote is held
+    }
+
+    public void IncreaseAngle()
+    {
+        // TO-DO!!
+        // Create script to only call the AddCannonAngle method on the cannon if remote is held
+    }
+
+    public void DecreaseAngle()
+    {
+        // TO-DO!!
+        // Create script to only call the AddCannonAngle method on the cannon if remote is held
+    }
+}
